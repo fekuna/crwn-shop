2c31a08008aac7b4d109c270213838dde38ed79b
fekuna <hichirro@gmail.com>
2019-07-25 11:34:28 +0700

    React-router-dom added withRouter added also

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>Hello, it's hats page</div>
+)
+
+function App() {
+	return <div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop/hats' component={HatsPage} />
+    </Switch>
+  </div>;
+}
+
+export default App;
