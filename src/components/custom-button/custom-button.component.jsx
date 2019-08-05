1d8d7d1a34e19c7e8b60db36c519039dcca27dc2
fekuna <hichirro@gmail.com>
2019-08-05 19:10:07 +0700

    prepare to deploy to heroku

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+import './custom-buttom.styles.scss';
 
-const CustomButton = ({ children, isSignInWithGoogle, inverted, ...otherProps }) => (
-	<button
-		className={`${inverted ? 'inverted' : ''} ${isSignInWithGoogle ? 'google-sign-in' : ''} custom-button`}
-		{...otherProps}>
-		{children}
-	</button>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
 );
 
 export default CustomButton;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,78 +1,74 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
-class SignIn extends Component {
-	constructor(props) {
-		super(props);
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			email: '',
-			password: ''
-		};
-	}
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
 
-	handleChange = (event) => {
-		const { name, value } = event.target;
+  handleSubmit = async event => {
+    event.preventDefault();
 
-		this.setState({
-			[name]: value
-		});
-	};
+    const { email, password } = this.state;
 
-	handleSubmit = async (event) => {
-		event.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-		try {
-			const { email, password } = this.state;
-			await auth.signInWithEmailAndPassword(email, password);
-			this.setState({
-				email: '',
-				password: ''
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  handleChange = event => {
+    const { value, name } = event.target;
 
-	render() {
-		return (
-			<div className='sign-in'>
-				<h2 className='title'>I already have an account</h2>
-				<span>Sign in with your email and password</span>
+    this.setState({ [name]: value });
+  };
 
-				<form onSubmit={this.handleSubmit}>
-					<FormInput
-						type='email'
-						name='email'
-						value={this.state.email}
-						handleChange={this.handleChange}
-						label='Password'
-						required
-					/>
-					<FormInput
-						name='password'
-						type='password'
-						value={this.state.password}
-						handleChange={this.handleChange}
-						label='Password'
-						required
-					/>
-					<div className='buttons'>
-						<CustomButton type='submit'> SIGN IN </CustomButton>
-						<CustomButton onClick={signInWithGoogle} isSignInWithGoogle>
-							{' '}
-							Sign in with Google{' '}
-						</CustomButton>
-					</div>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
